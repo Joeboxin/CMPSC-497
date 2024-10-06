001e2df7da64c617aaf0e100c0141f7e2fba35ca
Joeboxin <joey.hong26036@gmail.com>
2024-10-06 18:27:02 -0400

    Multiconnection server

--- peer1.py
+++ peer1.py
@@ -1,10 +1,10 @@
 import socket
-HOST = '127.0.0.1' # The server's hostname or IP address
-PORT = 65432 # The port used by the server
+HOST = "127.0.0.1"
+PORT = 4009
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(b"Hello, world")
     data = s.recv(1024)
     s.close()
 
-print(f"Received’, {str(data)}, encoding='utf-8'")
+print(f"Received {str(data)}")
--- server.py
+++ server.py
@@ -1,47 +1,93 @@
-#import sys
+import sys
 import selectors
-#import types
 import socket
+import threading
+import types
 
 HOST = '127.0.0.1' # localhost
 PORT = 65432 # Port to listen on
-import threading
-socket_list = []
-s = socket.socket()
-s.bind(('127.0.0.1', 8001))
-s.listen()
-
-def socket_target(conn):
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.send(data)
-
-while True:
-    conn, address = s.accept()
-    socket_list.append(conn)
-    threading.Thread(target = socket_target, args = (ConnectionRefusedError,)).start
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
-s.close()
 
 sel = selectors.DefaultSelector()
-HOST = "127.0.0.1"
-PORT = 1024
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST,PORT))
-    s.listen()
-    print(f"Listening on {(HOST, PORT)}")
-    s.setblocking(False)
-    sel.register(s, selectors.EVENT_READ, data=None)
-    
+
+messages = [b"Message 1 from client.", b"Message 2 from client."]
+
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+        
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+host, port = HOST, PORT
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+
